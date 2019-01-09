bcd653c6dc5fb292e449ab1ea2731ca5439929b8
Claudio Salas <claudiosalas@outlook.com>
2019-01-09 01:36:55 -0200

    fixed some issues

--- routes/ubs.js
+++ routes/ubs.js
@@ -13,19 +13,27 @@ const parse = (page, records, path) => {
   }
   const startIndex = page === 1 ? 0 : page * limit - (limit + 1);
   const endIndex = page === 1 ? limit : (page * limit - 1) > records.length ? records.length - 1 : page * limit - 1;
-  const values = records.slice(startIndex, endIndex)
-  return {
+  const values = records.slice(startIndex, endIndex);
+  const data = {
     '_metadata': {
       'page': `${page} of ${maxNumberOfPages}`,
       'page_count': values.length,
       'start_index': page === 1 ? 1 : startIndex + 2,
       'end_index': page === 1 ? limit : endIndex + 1,
       'total_count': records.length,
-      'previous_page': `${baseUrl}/${path}/${page === 1 ? page : page - 1}`,
-      'next_page': `${baseUrl}/${path}/${page + 1}`
+      'current_page': `${baseUrl}/${path}/${page}`
     },
     'records': values
   }
+
+  if (page > 1) {
+    data['_metadata']['previous_page'] = `${baseUrl}/${path}/${page - 1}`;
+  }
+  if (page < maxNumberOfPages) {
+    data['_metadata']['next_page'] = `${baseUrl}/${path}/${page + 1}`;
+  }
+
+  return data;
 }
 
 const searchCity = (value, city) => {
