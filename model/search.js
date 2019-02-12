f90142eddb638f508a7ab7d906a645bfced1ead7
Vinícius Machuca <vinicius.machuca@gmail.com>
2019-02-12 12:29:20 -0200

    Fixed search by city response when no city was found.

--- model/search.js
+++ model/search.js
@@ -1,7 +1,7 @@
 const ubs = require('../repository/ubs')
 
 const city = (actual, expected) => {
-  return actual.dsc_cidade && actual.dsc_cidade.toLowerCase() === expected.toLowerCase();
+  return actual.dsc_cidade && actual.dsc_cidade.toLowerCase().includes(expected.toLowerCase());
 }
 
 const nearBy = (lat, long, radius = 30000) => {
